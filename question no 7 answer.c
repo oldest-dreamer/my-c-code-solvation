30ae5cb582d205f157ed946618dd655a9c935dc7
maksudul alam <sciencesaru30@gmail.com>
2026-07-10 00:12:31 +0600

    Create question no 7 answer.c

---
+++ question no 7 answer.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ float r,area;
+ float pi=3.1416;
+ printf("enter the radius value:\n");
+ scanf("%f", &r);
+ area= pi*r*r;
+ printf("area of circle:%f\n",area);
+    return 0;
+}

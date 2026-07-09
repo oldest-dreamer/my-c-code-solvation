18d82974d62a41a119bbc22f5b4ab53b100b4f1f
maksudul alam <sciencesaru30@gmail.com>
2026-07-09 23:49:52 +0600

    Create question no 6 answer.c

---
+++ question no 6 answer.c
@@ -0,0 +1,8 @@
+include<stdio.h>
+int main()
+{
+    printf("Department name:computer science and engineering\n");
+    printf("ID:2607036\n");
+    printf("institute name:national institute of textile engineering and research\n");
+    return 0;
+}
